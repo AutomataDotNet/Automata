d62a5850cad06203e30401aabc55e5101dbef8c7
lorisdanto <loris@cs.wisc.edu>
2016-06-17 14:57:21 -0500

    random formulas experiments

---
+++ src/MSO.Eval/GenRandomMSO.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Diagnostics;
+
+using Microsoft.Automata.MSO;
+using Microsoft.Automata;
+using Microsoft.Automata.Z3;
+using Microsoft.Automata.Z3.Internal;
+using Microsoft.Z3;
+using System.IO;
+using Microsoft.Automata.MSO.Mona;
+using System.Threading;
+
+namespace MSOEvaluation
+{
+    public class GenRandomMSO
+    {
+        static CartesianAlgebraBDD<BoolExpr> solver;
+        static MSOFormula<BoolExpr> formula;
+        static List<BoolExpr> predicates;
+        static void Minterm()
+        {
+            try
+            {
+                z3.GenerateMinterms(predicates.ToArray());
+            }
+            catch (ThreadAbortException)
+            {
+                // cleanup code, if needed...
+            }
+        }
+        static void CartesianSolver()
+        {
+            try
+            {
+                var aut = formula.GetAutomaton(solver);
+            }
+            catch (ThreadAbortException)
+            {
+                // cleanup code, if needed...
+            }
+        }
+
+        static long timeout = 10;
+
+        public static void Run(){
+
+            c = new Context();
+            z3 = new Z3BoolAlg(c, c.BoolSort);
+
+
+            foreach (var pair in GenerateMSOZ3Formulas(20, 1, 50))
+            {
+                formula = pair.First;
+                predicates = pair.Second;
+
+                var bdd = new BDDAlgebra();
+                solver = new CartesianAlgebraBDD<BoolExpr>(bdd,z3);
+                var sw = new Stopwatch();
+                sw.Restart();
+                Thread t = new Thread(CartesianSolver);
+                t.Start();
+                long t1 = timeout;
+                if (!t.Join(TimeSpan.FromSeconds(timeout)))
+                {
+                    t.Abort();
+                    t1 = timeout;
+                }
+                else {
+                    sw.Stop();
+                    t1 = sw.ElapsedMilliseconds;
+                }
+
+
+                sw.Restart();
+                t = new Thread(Minterm);
+                t.Start();
+                long t2 = timeout;
+                if (!t.Join(TimeSpan.FromSeconds(timeout)))
+                {
+                    t.Abort();
+                    t2 = timeout;
+                }
+                else {
+                    sw.Stop();
+                    t2 = sw.ElapsedMilliseconds;
+                }
+
+                Console.WriteLine((double)t1  + "," + (double)t2);
+            }
+        }
+
+
+        const bool app = false;        
+        
+        static int size;                 //Numb of states (computed randomly)
+        static int alphVars;
+        static Random random;
+        static int seed;
+        static int totgenerations;
+
+        static Z3BoolAlg z3;
+        static Context c;
+
+
+        //Examples generated with params (6,15,1000), (10,12,1000),
+        public static IEnumerable<Pair<MSOFormula<BoolExpr>, List<BoolExpr>>> GenerateMSOZ3Formulas(int maxS, int numVarsinAlphabet, int totGen)
+        {                 
+            totgenerations = totGen;
+            alphVars = numVarsinAlphabet;
+
+            
+
+            for (seed = 0; seed < totgenerations; seed++)
+            {
+                Console.WriteLine(seed);
+                random = new Random(seed);
+                size = maxS;
+
+                var pair =  GenerateMSOFormula(1);       
+                yield return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(
+                    new MSOExists<BoolExpr>(new Variable("x" + 0, true), pair.First), pair.Second);
+            }
+        }
+
+        private static Pair<MSOFormula<BoolExpr>, List<BoolExpr>> GenerateMSOFormula(int maxVarIndex)
+        {
+            int randomNumber = random.Next(0, 6);
+            size--;
+            if (size == 0)
+            {
+                int variable = random.Next(0, maxVarIndex-1);
+                BoolExpr b = GeneratePredicate();
+                List<BoolExpr> l = new List<BoolExpr>();
+                l.Add(b);
+                return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(new MSOPredicate<BoolExpr>(b, new Variable("x"+variable, true)), l);
+            }
+            switch (randomNumber)
+            {
+                case 0:
+                    {
+                        Pair<MSOFormula<BoolExpr>, List<BoolExpr>> phi1 = GenerateMSOFormula(maxVarIndex + 1);
+                        MSOFormula<BoolExpr> phi = new MSOExists<BoolExpr>(new Variable("x"+maxVarIndex, true), phi1.First);
+                        return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(phi, phi1.Second);
+                    }
+                case 1:
+                    {
+                        Pair<MSOFormula<BoolExpr>, List<BoolExpr>> phi1 = GenerateMSOFormula(maxVarIndex + 1);
+                        MSOFormula<BoolExpr> phi = new MSOForall<BoolExpr>(new Variable("x" + maxVarIndex, true), phi1.First);
+                        return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(phi, phi1.Second);
+                    }
+                case 2:
+                    {
+                        Pair<MSOFormula<BoolExpr>, List<BoolExpr>> phi1 = GenerateMSOFormula(maxVarIndex);
+                        Pair<MSOFormula<BoolExpr>, List<BoolExpr>> phi2 = GenerateMSOFormula(maxVarIndex);
+                        MSOFormula<BoolExpr> phi = new MSOAnd<BoolExpr>(phi1.First, phi2.First);
+                        return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(phi, new List<BoolExpr>(phi1.Second.Union(phi2.Second)));
+                    }
+                case 3:
+                    {
+                        Pair<MSOFormula<BoolExpr>, List<BoolExpr>> phi1 = GenerateMSOFormula(maxVarIndex);
+                        Pair<MSOFormula<BoolExpr>, List<BoolExpr>> phi2 = GenerateMSOFormula(maxVarIndex);
+                        MSOFormula<BoolExpr> phi = new MSOOr<BoolExpr>(phi1.First, phi2.First);
+                        return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(phi, new List<BoolExpr>(phi1.Second.Union(phi2.Second)));
+                    }
+                case 4:
+                    {
+                        Pair<MSOFormula<BoolExpr>, List<BoolExpr>> phi1 = GenerateMSOFormula(maxVarIndex);
+                        MSOFormula<BoolExpr> phi = new MSONot<BoolExpr>(phi1.First);
+                        return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(phi, phi1.Second);
+                    }
+                case 5:
+                    {
+                        if (maxVarIndex > 1)
+                        {
+                            int variable1 = random.Next(0, maxVarIndex - 1);
+                            int variable2 = random.Next(0, maxVarIndex - 1);
+                            if (variable1 == variable2)
+                            {
+                                if (variable1 == maxVarIndex - 1)
+                                    variable1 = variable1 - 1;
+                                else
+                                    variable2 = variable2 + 1;
+                            }
+
+                            //Successor
+                            MSOFormula<BoolExpr> phi = new MSOSuccN<BoolExpr>(varOf(variable1),varOf(variable2),random.Next(1,4));
+                            return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(phi, new List<BoolExpr>());
+                        }
+                        else
+                        {
+                            int variable = random.Next(0, maxVarIndex - 1);
+                            BoolExpr b = GeneratePredicate();
+                            List<BoolExpr> l = new List<BoolExpr>();
+                            l.Add(b);
+                            return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(new MSOPredicate<BoolExpr>(b, new Variable("x" + variable, true)), l);
+                        }
+
+                    }
+                case 6:
+                    {
+                        int variable1 = random.Next(0, maxVarIndex - 1);
+                        int variable2 = random.Next(0, maxVarIndex - 1);
+
+                        //less than
+                        MSOFormula<BoolExpr> phi = new MSOLe<BoolExpr>(varOf(variable1), varOf(variable2));
+                        return new Pair<MSOFormula<BoolExpr>, List<BoolExpr>>(phi, new List<BoolExpr>());
+                    }
+            }
+            return null;
+        }
+        
+        private static Variable varOf(int n)
+        {
+            return new Variable("x" + n, true);
+        }
+
+        #region expressions and predicates generator
+        private static BoolExpr GeneratePredicate()
+        {
+            switch (random.Next(0, 4))
+            {
+                case 0:
+                    {
+                        IntExpr e1 = GenerateExprOfNumb();
+                        IntExpr e2 = GenerateExprOfNumb();
+                        switch (random.Next(0, 5))
+                        {
+                            case 0:
+                                {
+                                    return c.MkEq(e1, e2);
+                                }
+                            case 1:
+                                {
+                                    return c.MkGe(e1, e2);
+                                }
+                            case 2:
+                                {
+                                    return c.MkGt(e1, e2);
+                                }
+                            case 3:
+                                {
+                                    return c.MkLe(e1, e2);
+                                }
+                            case 4:
+                                {
+                                    return c.MkLt(e1, e2);
+                                }
+                        }                        
+                        break;
+                    }
+                case 1:
+                    {
+                        var v = random.Next(0, 4);
+
+                        BoolExpr e1 = GeneratePredicate();
+                        switch (v)
+                        {
+                            case 0:
+                                {
+                                    BoolExpr e2 = GeneratePredicate();
+                                    return c.MkAnd(e1, e2);
+                                }
+                            case 1:
+                                {
+                                    BoolExpr e2 = GeneratePredicate();
+                                    return c.MkOr(e1, e2);
+                                }
+                            case 2:
+                                {
+                                    return c.MkNot(e1);
+                                }
+
+                        }
+                        return e1;
+                    }
+                case 2:
+                case 3:
+                    {
+                        break;
+                    }
+            }
+            return c.MkTrue();
+        }
+
+        private static IntExpr GenerateExprOfNumb()
+        {
+            int randomNumber = random.Next(0, 4);
+            switch (randomNumber)
+            {
+                case 0:
+                case 1:
+                    {
+                        return (IntExpr)(c.MkConst("y"+random.Next(0,1),c.IntSort));
+                    }
+                case 2:
+                    {
+                        var e1 = GenerateExprOfNumb();
+                        var e2 = GenerateExprOfNumb();
+                        return (IntExpr)(c.MkAdd(e1, e2));
+                    }
+                case 3:
+                case 4:
+                    {
+                        return c.MkInt(random.Next(0, 9999));
+                    }
+            }
+            throw new Exception("this shouldn't happen");
+        }
+        #endregion
+        
+    }
+}
--- src/MSO.Eval/Program.cs
+++ src/MSO.Eval/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.Automata.Z3.Internal;
 using Microsoft.Z3;
 using System.IO;
 using Microsoft.Automata.MSO.Mona;
+using System.Threading;
 
 namespace MSOEvaluation
 {
@@ -30,7 +31,16 @@ namespace MSOEvaluation
         /// </summary>
         public static void Main()
         {
-            LTLTest();
+            GenRandomMSO.Run();
+
+            string outF = @"..\ws1stest.txt";
+            string inpD = @"C:\github\automatark\ws1s\";
+
+            //LTLTest(inpD,outF);
+
+            //outF = @"..\ltltest.txt";
+            //inpD = @"C:\github\automatark\m2l-str\";
+            //LTLTest(inpD, outF);
 
 
             //POPLTestsNew();
@@ -43,46 +53,120 @@ namespace MSOEvaluation
             //Console.Read();
         }
 
+
+        static MSOFormula<BDD> phi;
+        static BDDAlgebra solver;
+        static CharSetSolver cartSolver;
+        static void BDDSolver()
+        {
+            try
+            {
+                var aut = phi.GetAutomaton(solver);
+            }
+            catch (ThreadAbortException)
+            {
+                // cleanup code, if needed...
+            }
+        }
+
+        static void CartesianSolver()
+        {
+            try
+            {
+                var aut = phi.GetAutomaton(cartSolver);
+            }
+            catch (ThreadAbortException)
+            {
+                // cleanup code, if needed...
+            }
+        }
+        public static int cmp(string s1, string s2)
+        {
+            var s1splits = s1.Split('\\');
+            var s2splits = s2.Split('\\');
+
+            var dir1 = s1splits[s1splits.Length - 2];
+            var dir2 = s2splits[s2splits.Length - 2];
+            if (dir1.CompareTo(dir2) < 0)
+                return -1;
+            if (dir1.CompareTo(dir2) > 0)
+                return 1;
+            
+
+
+            var s1c = s1splits[s1splits.Length - 1].ToCharArray();
+            var s2c = s2splits[s2splits.Length - 1].ToCharArray();
+
+
+            int ll = 15;
+            if (s1c[0] == 'P' && s2c[0] == 'P')
+            {
+                var pref1 = s1splits[s1splits.Length - 1].Substring(0, s1splits[s1splits.Length - 1].IndexOf("form"));
+                var pref2 = s2splits[s2splits.Length - 1].Substring(0, s1splits[s1splits.Length - 1].IndexOf("form"));
+                if (pref1.CompareTo(pref2) != 0)
+                    return pref1.CompareTo(pref2);
+
+
+                var ind1 = s1splits[s1splits.Length - 1].IndexOf("form") + 4;
+                var ind2 = s2splits[s2splits.Length - 1].IndexOf("form") + 4;
+                var i1 = int.Parse(s1splits[s1splits.Length - 1].Substring(s1splits[s1splits.Length - 1].IndexOf("form")+4, s1splits[s1splits.Length - 1].Length-5-ind1));
+                var i2 = int.Parse(s2splits[s2splits.Length - 1].Substring(s2splits[s2splits.Length - 1].IndexOf("form") + 4, s2splits[s2splits.Length - 1].Length - 5-ind2));
+                return i1 - i2;
+            }
+
+            return s1.CompareTo(s2);
+
+            
+        }
+
+
         //LTL over finite traces
-        public static void LTLTest()
+        public static void LTLTest(string inputDir, string outFile)
         {
             using (System.IO.StreamWriter file =
-               new System.IO.StreamWriter(@"..\ltltest.txt"))
+               new System.IO.StreamWriter(@"..\ws1stest.txt"))
             {
-                foreach (string fileName in Directory.EnumerateFiles(@"C:\github\automatark\m2l-str\LTL-finite", "*.mona", SearchOption.AllDirectories))
+                var files = new List<string>(Directory.EnumerateFiles(@"C:\github\automatark\ws1s\", "*.mona", SearchOption.AllDirectories));
+                files.Sort( (s1, s2) => cmp(s1, s2));
+                foreach (string fileName in files)
                 {
                     string contents = File.ReadAllText(fileName);
                     MonaProgram pgm1 = MonaParser.Parse(contents);
-                    var phi = pgm1.ToMSO();
+                    phi = pgm1.ToMSO();
 
-                    var solver = new BDDAlgebra(); //CharSetSolver(BitWidth.BV64);
+                    solver = new BDDAlgebra(); //CharSetSolver(BitWidth.BV64);
                     var sw = new Stopwatch();
                     sw.Restart();
-                    for (int t = 0; t < numTests; t++)
+                    Thread t = new Thread(BDDSolver);
+                    t.Start();
+                    long t1=5000;
+                    if (!t.Join(TimeSpan.FromSeconds(5)))
                     {
-                        var aut = phi.GetAutomaton(solver);
-                        //aut.ShowGraph();
+                        t.Abort();
+                        t1 = 5000;
                     }
-
-                    sw.Stop();
-
-                    var t1 = sw.ElapsedMilliseconds;
-
+                    else {
+                        sw.Stop();
+                        t1 = sw.ElapsedMilliseconds;
+                    }
 
                     //var newSolver = new CharSetSolver(BitWidth.BV64);
 
-                    //sw.Restart();
-                    //for (int t = 0; t < numTests; t++)
+                    sw.Restart();
+                    //t = new Thread(CartesianSolver);
+                    //t.Start();
+                    //if (!t.Join(TimeSpan.FromSeconds(5)))
                     //{
                     //    phi.GetAutomaton(newSolver);
-                    //}
+                    //}                  
                     //sw.Stop();
 
-                    //var t2 = sw.ElapsedMilliseconds;
+                    var t2 = sw.ElapsedMilliseconds;
+                    //if (t2 > 5000)
+                    //    t2 = 5000;
 
-                    //file.WriteLine(fileName + "," + (double)t1 / numTests + "," + (double)t2 / numTests);
-                    //Console.WriteLine(fileName + "," + (double)t1 / numTests + "," + (double)t2 / numTests);
-                    Console.WriteLine(fileName + "," + (double)t1 / numTests);
+                    file.WriteLine(fileName + "," + (double)t1 / numTests +"," + (double)t2 / numTests);
+                    Console.WriteLine(fileName + "," + (double)t1 / numTests + "," + (double)t2 / numTests);
                 }
             }
         }
